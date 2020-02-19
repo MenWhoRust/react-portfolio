a5cddbc8b5fa7091dab6bfc7d9679f4322676187
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2020-02-19 16:31:19 +0200

    tested centering and added image pin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./styles/index.scss"
+import ImagePin from "./components/image-pin";
 
 function App() {
   return (
-    <div className="">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="test-container">
+          <ImagePin src={}/>
+        <h1 className="heading-primary">Hello</h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/image-pin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function ImagePin(props) {
+    let imgUrl = props.src;
+    //TODO: Add image element
+    return(
+        <div></div>
+    )
+
+}
+
+export default ImagePin;
