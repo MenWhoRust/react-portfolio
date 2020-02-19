0400f7a257ea1eb1744f43cf80fe09ce51afbb3d
Phabiann-Ross De Carvalho <prdecarvalho96@gmail.com>
2020-02-19 20:43:59 +0200

    Added img to image-pin component and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles/index.scss"
 import ImagePin from "./components/image-pin";
+import pic from "./resources/images/27804703.jpeg"
 
 function App() {
   return (
     <div className="app-container">
       <div className="test-container">
-          <ImagePin src={}/>
+          <ImagePin src={pic}/>
         <h1 className="heading-primary">Hello</h1>
       </div>
     </div>
--- src/components/image-pin.jsx
+++ src/components/image-pin.jsx
@@ -3,9 +3,12 @@ import React from "react";
 
 function ImagePin(props) {
     let imgUrl = props.src;
+    let alt = props.alt;
     //TODO: Add image element
     return(
-        <div></div>
+        <div className="image-pin image-pin--large">
+            <img className="image-content" alt={alt} src={imgUrl}/>
+        </div>
     )
 
 }
