bc5932b7e15b73d47764eb5010177f89563f2cda
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2020-02-25 20:56:41 +0200

    Added react-markdown

--- src/components/hero-modal.jsx
+++ src/components/hero-modal.jsx
@@ -14,6 +14,7 @@ const HeroModal = (props) => {
                 <FontAwesomeIcon icon={faTimes}/>
             </div>
             <div className={getCurrentModalClass('hero-modal__content', props.isOpen)}>
+                {props.content}
             </div>
         </div>
     )
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import summaryContent from "../resources/json/summary-content.json"
+import ReactMarkdown from "react-markdown";
 
 function Summary() {
 
     return(
       <div className={"summary__container theme-dark"}>
+          <ReactMarkdown className={"summary_content theme-dark"}>
           {summaryContent.content}
+          </ReactMarkdown>
       </div>
     );
 
