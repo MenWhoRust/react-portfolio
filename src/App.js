45255ac07f468f655f21c4d12b00f5b542e533d1
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2020-05-22 18:05:21 +0200

    gpg test

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
-import React, {Component} from "react";
-import "./styles/index.scss"
+import React, { Component } from "react";
+import "./styles/index.scss";
 import MainMenu from "./components/main-menu";
 import NavButton from "./components/nav-button";
 import HeroModal from "./components/hero-modal";
 import Summary from "./components/summary";
 
 class App extends Component {
+  modalMapping = {
+    Summary: <Summary />,
+    Skills: null,
+    Projects: null,
+    Aspirations: null,
+  };
 
-    modalMapping = {
-        Summary: <Summary/>,
-        Skills: null,
-        Projects: null,
-        Aspirations: null
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activateHero: false,
+      heroModalOpen: false,
+      heroModalContent: "",
     };
+  }
 
-    constructor(props) {
-        super(props);
+  destroyHero() {
+    this.setState({ heroModalOpen: false });
 
-        this.state = {
-            activateHero: false,
-            heroModalOpen: false,
-            heroModalContent: ''
-        };
-    }
+    setTimeout(() => this.setState({ activateHero: false }), 1000);
+  }
 
-    destroyHero(){
-        this.setState({heroModalOpen: false})
-
-        setTimeout(() => this.setState({activateHero: false}), 1000)
-    }
-
-
-
-
-    render() {
-        return (
-            <div className="app-container theme-dark">
-                {this.state.activateHero ? <HeroModal isOpen={this.state.heroModalOpen} content={this.modalMapping[this.state.heroModalContent]} closeOnClick={() => this.destroyHero()}/> : ""}
-                <MainMenu>
-                    {
-                        Object.keys(this.modalMapping).map((c, i) => <NavButton key={i} text={c} onClick={() => this.setState({
-                            activateHero:true,
-                            heroModalOpen: true,
-                            heroModalContent: c
-                        })}/>)
-                    }
-                </MainMenu>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='app-container theme-dark'>
+        {this.state.activateHero ? (
+          <HeroModal
+            isOpen={this.state.heroModalOpen}
+            content={this.modalMapping[this.state.heroModalContent]}
+            closeOnClick={() => this.destroyHero()}
+          />
+        ) : (
+          ""
+        )}
+        <MainMenu>
+          {Object.keys(this.modalMapping).map((c, i) => (
+            <NavButton
+              key={i}
+              text={c}
+              onClick={() =>
+                this.setState({
+                  activateHero: true,
+                  heroModalOpen: true,
+                  heroModalContent: c,
+                })
+              }
+            />
+          ))}
+        </MainMenu>
+      </div>
+    );
+  }
 }
 
 export default App;
