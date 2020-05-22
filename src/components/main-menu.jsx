61b56e9dc19da8129f58fa9fa3cb1c19801865d9
Phabiann-Ross De Carvalho <prdecarvalho96@gmail.com>
2020-05-22 17:53:23 +0200

    Formatted files

--- src/components/main-menu.jsx
+++ src/components/main-menu.jsx
@@ -5,17 +5,15 @@ import PhraseRevolver from "./phrase-revolver";
 import phrasesJson from "../resources/json/phrases";
 
 const MainMenu = (props) => {
-    return (
-        <div className="main-menu__container theme-dark">
-            <ImagePin src={pic} alt="Me!"/>
-            <PhraseRevolver text="I love $phrases!" phrases={phrasesJson}/>
-            <div className="main-menu__button-box theme-dark">
-                {
-                    props.children.filter(c => c.type.name === 'NavButton')
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div className='main-menu__container theme-dark'>
+      <ImagePin src={pic} alt='Me!' />
+      <PhraseRevolver text='I love $phrases!' phrases={phrasesJson} />
+      <div className='main-menu__button-box theme-dark'>
+        {props.children.filter((c) => c.type.name === "NavButton")}
+      </div>
+    </div>
+  );
 };
 
 export default MainMenu;
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import md from "../resources/md/summary.md"
+import md from "../resources/md/summary.md";
 
-class Summary extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {markdown: ''};
+class Summary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { markdown: "" };
 
-        fetch(md).then(res => res.text()).then(txt => this.setState({markdown: txt}));
-    }
+    fetch(md)
+      .then((res) => res.text())
+      .then((txt) => this.setState({ markdown: txt }));
+  }
 
-
-    render() {
-        return (
-            <div className={"summary__container theme-dark"}>
-                <ReactMarkdown className={"summary__content theme-dark"} source={this.state.markdown}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={"summary__container theme-dark"}>
+        <ReactMarkdown className={"summary__content theme-dark"} source={this.state.markdown} />
+      </div>
+    );
+  }
 }
 
 export default Summary;
