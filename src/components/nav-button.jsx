05399c12e1f341bc9b927ddaab12e309a75c3df3
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2020-05-22 18:01:49 +0200

    gpg test

--- src/components/nav-button.jsx
+++ src/components/nav-button.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 function NavButton(props) {
-    return(
-        <div className="nav-button__container theme-dark" onClick={props.onClick}>
-            <span className="nav-button__text theme-dark">{props.text}</span>
-        </div>
-    )
-
+  return (
+    <div className='nav-button__container theme-dark' onClick={props.onClick}>
+      <span className='nav-button__text theme-dark'>{props.text}</span>
+    </div>
+  );
 }
 
 export default NavButton;
